b0cfd2568ffd0f756c7ce0b7b6f9a6e85a5c9f6d
pi22by7 <70760826+pi22by7@users.noreply.github.com>
2022-07-03 16:02:39 +0530

    Chapter 1 complete.

---
+++ Chapter 1.cpp
@@ -0,0 +1,65 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+// image
+
+int image() 
+{
+	string path = "test.jpg";
+	Mat img = imread(path);
+	imshow("Image", img);
+	waitKey(0);
+
+	return 0;
+}
+
+
+// Video
+
+int video() 
+{
+
+	string path = "test.mp4";
+	VideoCapture cap(path);
+	Mat img;
+
+	while (true) 
+	{
+		cap.read(img);
+		imshow("Image", img);
+		waitKey(20);
+	}
+	return 0;
+}
+
+
+//Camera
+
+int camera() 
+{
+	VideoCapture cap(0);
+	Mat img;
+
+	while (true) 
+	{
+		cap.read(img);
+		imshow("Image", img);
+		waitKey(1);
+	}
+	return 0;
+}
+
+int main(void)
+{
+	//image();
+	//video();
+	camera();
+
+	return 0;
+}
